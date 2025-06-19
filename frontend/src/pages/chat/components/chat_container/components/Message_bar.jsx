5774acd7b196303705cd7e0e475f2d4ff32dcc4b
Jatin Singh <jatinsingh098loq2@outlook.com>
2025-06-19 21:28:21 +0530

    Base Chat Page done

---
+++ frontend/src/pages/chat/components/chat_container/components/Message_bar.jsx
@@ -0,0 +1,71 @@
+import EmojiPicker from "emoji-picker-react";
+import React, { useEffect, useRef, useState } from "react";
+import { GrAttachment } from "react-icons/gr";
+import { IoSend } from "react-icons/io5";
+import { RiEmojiStickerLine } from "react-icons/ri";
+
+const Message_bar = () => {
+  const emojiRef = useRef();
+  const [message, setMessage] = useState("");
+  const [showEmojiPickerOpen, setShowEmojiPickerOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (emojiRef.current && !emojiRef.current.contains(event.target)) {
+        setShowEmojiPickerOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [emojiRef]);
+
+  const handleAddEmoji = (emoji) => {
+    setMessage((msg) => msg + emoji.emoji);
+  };
+
+  const handleSendMessage = () => {};
+
+  return (
+    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6">
+      <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5">
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          name=""
+          className="flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none"
+          placeholder="Type your message here..."
+        />
+        <button className="text-neutral-500 focus:outline-none focus:border-none focus:text-white duration-300 transition-all">
+          <GrAttachment className="text-2xl" />
+        </button>
+        <div className="relative">
+          <button
+            className="text-neutral-500 focus:outline-none focus:border-none focus:text-white duration-300 transition-all"
+            onClick={() => setShowEmojiPickerOpen(!showEmojiPickerOpen)}
+          >
+            <RiEmojiStickerLine className="text-2xl" />
+          </button>
+          <div className="absolute bottom-16 right-0" ref={emojiRef}>
+            <EmojiPicker
+              theme="dark"
+              open={showEmojiPickerOpen}
+              onEmojiClick={handleAddEmoji}
+              autoFocusSearch={false}
+            />
+          </div>
+        </div>
+      </div>
+      <button
+        className="bg-[#8417ff] rounded-md flex items-center justify-center p-5 hover:bg-[#741bda] focus:outline-none focus:border-none focus:bg-[#741bda] duration-300 transition-all"
+        onClick={handleSendMessage}
+      >
+        <IoSend className="text-2xl" />
+      </button>
+    </div>
+  );
+};
+
+export default Message_bar;
---
+++ frontend/src/pages/chat/components/chat_container/components/Message_container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Message_container = () => {
+  return (
+    <div className="flex-1 overflow-y-auto scrolbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full ">
+      Message_container
+    </div>
+  );
+};
+
+export default Message_container;
